31cfa8a877acf18460b115b005541ac0bc78813e
Dan Victor <danVictor20@gmail.com>
2025-07-03 19:38:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/shooting-day-card.tsx
+++ src/components/shooting-day-card.tsx
@@ -22,7 +22,7 @@ import { Badge } from "./ui/badge";
 import { Separator } from "./ui/separator";
 import { WeatherCard } from "./weather-card";
 import { Skeleton } from "./ui/skeleton";
-import { AccordionContent, AccordionItem, AccordionTrigger } from "./ui/accordion";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./ui/accordion";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./ui/table";
 
 const DisplayMap = dynamic(() => import('./display-map').then(mod => mod.DisplayMap), {
