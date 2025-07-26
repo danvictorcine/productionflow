480ad2602f2d5eef38a660d7c95dfc01275428c1
Dan Victor <danVictor20@gmail.com>
2025-07-26 13:41:21 +0000

    Ainda continuam muito pequenos

--- src/app/storyboard/[id]/page.tsx
+++ src/app/storyboard/[id]/page.tsx
@@ -494,7 +494,7 @@ function StoryboardPageDetail() {
                             }}
                          >
                             <div ref={exportRef} className="p-8">
-                                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                                     {panels.map((panel, index) => (
                                         <PanelCard 
                                             key={panel.id} 
