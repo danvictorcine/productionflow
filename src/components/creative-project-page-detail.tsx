965ed79d20c4ca5e25702851dc5eac0baa16cb49
Dan Victor <danVictor20@gmail.com>
2025-08-14 02:41:55 +0000

    A div que deveria exibir os itens deveria ser assim:
    <div class="flex-1

--- src/components/creative-project-page-detail.tsx
+++ src/components/creative-project-page-detail.tsx
@@ -593,10 +593,9 @@ export default function CreativeProjectPageDetail({ project, initialItems, onDat
         </div>
       </div>
 
-      <div className="flex-1 overflow-hidden relative">
+      <div className="flex-1 overflow-hidden relative" ref={boardContainerRef}>
         <div
-          ref={boardContainerRef}
-          className="absolute inset-0 z-0 bg-grid-slate-200/[0.5] dark:bg-grid-slate-700/[0.5] cursor-grab active:cursor-grabbing"
+          className="absolute inset-0 z-10 bg-grid-slate-200/[0.5] dark:bg-grid-slate-700/[0.5] cursor-grab active:cursor-grabbing"
           onMouseDown={handleMouseDown}
           onMouseMove={handleMouseMove}
           onMouseUp={handleMouseUp}
